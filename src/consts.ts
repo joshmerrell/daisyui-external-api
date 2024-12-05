5825cbc9de9bc6728a33faea082d72d005b2d847
Joshua Merrell <jmerrell2@ewu.edu>
2024-12-05 12:07:30 -0800

    Move more stuff to imports

--- src/consts.ts
+++ src/consts.ts
@@ -1,4 +1,4 @@
-export const SITE_TITLE = "Astro with Daisy UI (a Tailwind Plugin)";
+export const SITE_TITLE = "D&D 5e Ability Scores";
 export const API_URL_START = "https://www.dnd5eapi.co/api";
 export const ABILITY_SCORE_INDICES = ["str","dex","con","int","wis","cha"];
-export const REPO_ALT = "Go to Josh's Github repo";
+export const REPO_URL = "https://github.com/joshmerrell/daisyui-external-api"
